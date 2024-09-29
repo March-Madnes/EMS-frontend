bea742ac024ff0fec0e5cf41283b3e10484de96c
Harshal141 <harshalmukundapatil@gmail.com>
2024-09-29 10:45:26 +0530

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      {/* <Route path="/build" element={<Builder />} /> */}
     </Routes>
   );
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const Landing = () => {
   return (
-    <div>Landing</div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
